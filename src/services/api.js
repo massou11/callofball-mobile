838d89933468c79b92580ae22820f22c2c8f14c9
Call of Ball <mass641@gmail.com>
2026-06-06 23:32:21 +0200

    Production build - Railway backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = process.env.EXPO_PUBLIC_API_URL || 'https://callofball-backend-production.up.railway.app/api';
 
 const api = axios.create({ baseURL: API_URL });
 
